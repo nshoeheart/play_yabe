11aa469a199682f9325614893f3350f781fdcb27
nshoeheart <nathan-schuchert@uiowa.edu>
2013-12-30 14:14:07 -0600

    added classes User and BasicTest

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -6,8 +6,26 @@ import models.*;
 public class BasicTest extends UnitTest {
 
     @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
+    public void createAndRetrieveUser() {
+        // Create a new user and save it
+        new User("ross@gmail.com", "1234", "Ross").save();
+
+        // Retrieve the user with email ross@gmail.com
+        User ross = User.find("byEmail", "ross@gmail.com").first();
+
+        // Test
+        assertNotNull(ross);
+        assertEquals("Ross", ross.fullname);
+    }
+    @Test
+    public void tryConnectAsUser() {
+        // Create a new user and save it
+        new User("ross@gmail.com", "1234", "Ross").save();
+
+        //Test
+        assertNotNull(User.connect("ross@gmail.com", "1234"));
+        assertNull(User.connect("ross@gmail.com", "not1234"));
+        assertNull(User.connect("notross@gmail.com", "1234"));
     }
 
 }
