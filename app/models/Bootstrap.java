acf05a29f8f9b2ed80ba2788b15231e83c2c7e17
nshoeheart <nathan-schuchert@uiowa.edu>
2013-12-31 11:14:44 -0600

    First homepage build

---
+++ app/models/Bootstrap.java
@@ -0,0 +1,18 @@
+package models;
+
+import play.*;
+import play.jobs.*;
+import play.test.*;
+
+import models.*;
+
+@OnApplicationStart
+public class Bootstrap extends Job {
+
+    public void doJob() {
+        //Check if database is empty
+        if(User.count() == 0) {
+            Fixtures.loadModels("initial-data.yml");
+        }
+    }
+}
