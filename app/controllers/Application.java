0bfcf535cc127898a1a5bc124279ba379698d57d
nshoeheart <nathan-schuchert@uiowa.edu>
2014-01-02 09:59:36 -0600

    functional Captcha in comments

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -27,16 +27,32 @@ public class Application extends Controller {
 
     public static void show(Long id) {
         Post post = Post.findById(id);
-        render(post);
+        String randomID = Codec.UUID();
+        render(post, randomID);
     }
 
-    public static void postComment(Long postID, @Required String author, @Required String content) {
+    public static void postComment(
+            Long postID,
+            @Required(message="Your name is required") String author,
+            @Required(message="A message is required") String content,
+            @Required(message="Please type the code") String code,
+            String randomID)
+    {
         Post post = Post.findById(postID);
+        validation.equals(code, Cache.get(randomID)).message("Invalid code. Please type it again");
         if (validation.hasErrors()) {
-            render("Application/show.html", post);
+            render("Application/show.html", post, randomID);
         }
         post.addComment(author, content);
         flash.success("Thanks for posting, %s", author);
+        Cache.delete(randomID);
         show(postID);
+    }
+
+    public static void captcha(String id) {
+        Images.Captcha captcha = Images.captcha();
+        String code = captcha.getText("#E4EAFD");
+        Cache.set(id, code, "10mn");
+        renderBinary(captcha);
     }
 }
