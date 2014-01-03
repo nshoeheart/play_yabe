c2f939eb35b602e5d30367de87c5c8c5116915f6
nshoeheart <nathan-schuchert@uiowa.edu>
2014-01-03 11:03:23 -0600

    altered Security controller

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -2,7 +2,8 @@ package controllers;
 
 import models.*;
 
-public class Security extends Secure.Security {
+public class Security extends Secure
+        .Security {
 
     static boolean authenticate(String username, String password) {
         return User.connect(username, password) != null;
